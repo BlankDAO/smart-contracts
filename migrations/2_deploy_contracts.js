201739b0e986419b362872f92e26b40b2fff9cf8
siftal <m.khanmohamadzadeh@gmail.com>
2019-02-08 19:27:18 +0330

    change Minter to BlankMinter

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var BlankToken = artifacts.require('BlankToken.sol');
-var Minter = artifacts.require('Minter.sol');
+var BlankMinter = artifacts.require('BlankMinter.sol');
 var BlankCrowdsale = artifacts.require('BlankCrowdsale.sol');
 var cap = 21*10**24
 
@@ -10,10 +10,10 @@ module.exports = function (deployer) {
     await deployer.deploy(BlankToken,cap);
     const instanceBlankToken = await BlankToken.deployed();
 
-    await deployer.deploy(Minter, instanceBlankToken.address);
-    const instanceMinter = await Minter.deployed();
+    await deployer.deploy(BlankMinter, instanceBlankToken.address);
+    const instanceBlankMinter = await BlankMinter.deployed();
 
-    await instanceBlankToken.transferOwnership(instanceMinter.address);
+    await instanceBlankToken.transferOwnership(instanceBlankMinter.address);
 
     await deployer.deploy(BlankCrowdsale, instanceBlankToken.address, 0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359);
     const instanceBlankCrowdsale = await BlankCrowdsale.deployed();
